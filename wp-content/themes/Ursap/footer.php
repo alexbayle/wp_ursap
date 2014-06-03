6cd4d6105c8318c1d70515effb729fb45a0a9ebd
Bayle Alexandre <alex.bayle.6691@gmail.com>
2014-06-03 12:58:32 +0200

    maj

--- wp-content/themes/Ursap/footer.php
+++ wp-content/themes/Ursap/footer.php
@@ -2,9 +2,14 @@
 
 ?>
 </div>
-<footer id="colophon" role="contentinfo">
-    <div class="site-info">
+<div class="container" id="footer">
+    <div class="row">
+        <div class="col-md-12">
 
+        </div>
+        <div class="col-md-12">
+            &copy; Copyright Ursap - Tous droits réservés
+        </div>
     </div>
 </footer>
 </div>
