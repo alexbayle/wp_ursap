7b311489fc37e7a0adbbeb2c21b52dad1fb31bfb
Bayle Alexandre <alex.bayle.6691@gmail.com>
2014-06-03 16:55:11 +0200

    footer

--- wp-content/themes/Ursap/footer.php
+++ wp-content/themes/Ursap/footer.php
@@ -2,30 +2,32 @@
 
 ?>
 </div>
+</div>
 <footer>
         <div id="haut">
             <div class="logofooter">
                 <img src="" alt=""/>
             </div>
             <div class="menu1">
-                <a href="">A Propos</a>
+                <a href="">A Propos de Ursap</a>
                 <a href="">Prix</a>
                 <a href="">Contact</a>
             </div>
+            <div class="clear"></div>
         </div>
         <div id="bas">
             <div class="copyright">
                 <p>&copy; Copyright Ursap - Tous droits réservés</p>
             </div>
             <div class="menu2">
-                <p>
-                    <a href="">Terms of Service</a>
-                    <a href="">Privacy Polic</a>
-                </p>
+                <ul>
+                    <li><a href="">Terms of Service</a></li>
+                    <li><a href="">Privacy Policy</a></li>
+                <ul>
             </div>
         </div>
 </footer>
-</div>
+
 <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js" type="text/javascript"></script>
 
 <?php wp_footer(); ?>
