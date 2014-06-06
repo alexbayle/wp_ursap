7c4fe6bf00009db45c211dc9c6e6d906d0eeeb4d
Julien B <julien.bonnetaud@gmail.com>
2014-06-06 14:47:29 +0200

    rdy to style

--- wp-content/themes/Ursap/header.php
+++ wp-content/themes/Ursap/header.php
@@ -17,12 +17,12 @@
     <link rel="profile" href="http://gmpg.org/xfn/11" />
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css"/>
-    <link rel="stylesheet/less" type="text/css" href="<?php echo get_template_directory_uri(); ?>/style.less"/>
-    <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>"/>
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/js/less-1.7.0.min.js" type="text/javascript"></script>
-
     <?php wp_head(); ?>
+    <link rel="stylesheet/less" type="text/css" href="<?php echo get_template_directory_uri(); ?>/style.less"/>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/less-1.7.0.min.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>"/>
+
 </head>
 
 <body <?php body_class(); ?>>
